35da9268a73fd6dc99e5d4281290c8f0ac5c15a8
ojulio-dev <juliocontact15@gmail.com>
2025-04-22 12:55:22 -0300

    Criando nossa barra de navegação

--- index.php
+++ index.php
@@ -8,9 +8,34 @@
 
     <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
 </head>
-<body>
+<body class="bg-stone-950 text-stone-200">
     
-    Hello Word!!
+    <header class="bg-stone-900">
+
+        <nav class="mx-auto max-w-screen-lg flex justify-between px-8 py-4">
+
+            <div class="font-bold text-xl tracking-wide">Book Wise</div>
+
+            <ul class="flex space-x-4 font-bold">
+
+                <li><a href="/" class="text-lime-500">Explorar</a></li>
+                <li><a href="/meus-livros.php" class="hover:underline">Meus Livros</a></li>
+
+            </ul>
+
+            <ul>
+                <li><a href="/login.php">Fazer Login</a></li>
+            </ul>
+
+        </nav>
+
+    </header>
+
+    <main>
+
+        lista de livros
+
+    </main>
 
 </body>
 </html>
