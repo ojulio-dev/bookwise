be3aecedc5b1bd4455fe569476342eb80e1315a9
ojulio-dev <juliocontact15@gmail.com>
2025-04-30 16:10:43 -0300

    Encriptar as senhas

--- controllers/login.controller.php
+++ controllers/login.controller.php
@@ -23,19 +23,33 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $usuario = $database->query(
 
-        query: " select * from usuarios where email = :email and senha = :senha",
+        query: " select * from usuarios where email = :email",
 
         class: Usuario::class,
 
-        params: compact('email', 'senha')
+        params: compact('email')
 
     )->fetch();
 
     if ($usuario) {
 
+        $senhaDoPost = $_POST['senha'];
+
+        $senhaDoBanco = $usuario->senha;
+
+        if (! password_verify($senhaDoPost, $senhaDoBanco)) {
+
+            flash()->push('validacoes_login', ['Usuário ou senha estão incorretos!']);
+
+            header('Location: /login');
+
+            exit();
+
+        }
+
         $_SESSION['auth'] = $usuario;
 
-        flash()->push('mensagem', "Seja bem-vindo" . $usuario->nome . "!");
+        flash()->push('mensagem', "Seja bem-vindo " . $usuario->nome . "!");
 
         header("Location: /");
 
