d23da4d22a9ab7a2c937dcf7dcf4c02c5f5a6ce7
ojulio-dev <juliocontact15@gmail.com>
2025-05-01 00:34:57 -0300

    Evitando repetições de código

--- controllers/meus-livros.controller.php
+++ controllers/meus-livros.controller.php
@@ -8,6 +8,6 @@ if (! auth()) {
 
 }
 
-$livros = $database->query("select * from livros where usuario_id = :id", Livro::class, ['id' => auth()->id]);
+$livros = Livro::meus(auth()->id);
 
 view('meus-livros', compact('livros'));
